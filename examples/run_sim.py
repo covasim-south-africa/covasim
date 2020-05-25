82f70068f388b75ffc836d1c5630148f6300004e
Mudimu <mudime@unisa.ac.za>
2020-05-25 22:42:06 +0200

    Adding SA observed data

--- examples/run_sim.py
+++ examples/run_sim.py
@@ -10,6 +10,18 @@ do_plot = 1
 verbose = 1
 interv  = 1
 
+# Set filename if saving
+version  = 'v0'
+date     = '2020apr06'
+folder   = 'results'
+basename = f'{folder}/covasim_run_{date}_{version}'
+fig_path = f'{basename}.png'
+res_path = f'{folder}/covasim_results_{date}_{version}.xlsx'
+
+folder_data   = 'data'
+basename_data = f'{folder_data}/SA_observed_data_Fin'
+file_path = f'{basename_data}.xlsx'
+
 # Configure the sim -- can also just use a normal dictionary
 pars = sc.objdict(
     pop_size     = 10000,    # Population size
@@ -24,7 +36,7 @@ if interv:
     pars.interventions = cv.change_beta(days=45, changes=0.5)
 
 # Make, run, and plot the sim
-sim = cv.Sim(pars=pars)
+sim = cv.Sim(pars=pars, datafile = file_path, datacols = ['t', 'cum_recoveries', 'cum_infections', 'new_infections', 'cum_deaths', 'n_infectious', 'new_recoveries', 'new_deaths', 'date')
 sim.initialize()
 sim.run(verbose=verbose)
 if do_plot:
--- examples/simple.py
+++ examples/simple.py
@@ -3,6 +3,12 @@ Simplest possible Covasim usage example.
 '''
 
 import covasim as cv
-sim = cv.Sim()
+
+folder   = 'data'
+basename = f'{folder}/SA_observed_data_Fin'
+file_path = f'{basename}.xlsx'
+
+
+sim = cv.Sim(datafile = file_path, datacols = ['t', 'cum_recoveries', 'cum_infections', 'new_infections', 'cum_deaths', 'n_infectious', 'new_recoveries', 'new_deaths', 'date'])
 sim.run()
 sim.plot()
